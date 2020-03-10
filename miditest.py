19206659b9a625e7b873c75cb2f1cc401300974a
Joseph Li <13826851+joseph-x-li@users.noreply.github.com>
2020-03-10 15:58:32 -0400

    change to int encoding

--- miditest.py
+++ miditest.py
@@ -2,32 +2,46 @@ import mido
 from mido import MidiFile
 import serial
 from pprint import pprint
+import struct
 
 mid = MidiFile('../Marunouchi.mid')
 port = "/dev/ttyUSB0"
 
-defaultString = "711111111111111111111111111111111111111"
-messageString = list("700000000000000000000000000000000000000")
 
-x="700000000000000000000000000000000000000"
+def modMessages(firstMsg, secondMsg, note, onOff = False):
+    if not (note >= 53 and note <= 89): # ONLY NOTES F3(53) => F6(89)
+        return (firstMsg, secondMsg)
+    note = note - 53 # conversion to zero indexed
+    if note <= 30:
+        if onOff:
+            firstMsg = firstMsg | (1<<(30-note))
+        else:
+            firstMsg = firstMsg & ~((1<<(30-note)))
+    else:
+        if onOff:
+            secondMsg = secondMsg | (1<<(61-note))
+        else:
+            secondMsg = secondMsg & ~((1<<(61-note)))
+    return (firstMsg, secondMsg)
 
-USB = serial.Serial(port, 9600)
+message_1 = 0x80000000
+message_2 = 0x00000000
+
+USB = serial.Serial(port, 115200)
 USB.flushInput()
 
-USB.write(defaultString.encode('utf-8'))
 
 for msg in mid.play():
     if not (msg.type == 'note_on' or msg.type == 'note_off'):
         continue
     note = msg.note
-    if not (note >=53 and note <=89): # ONLY NOTES F3(53) => F6(89)
-        continue
-    if msg.type == 'note_on':
-        messageString[note-52] = '1'
-    else:
-        messageString[note-52] = '0'
-    USB.write((''.join(messageString)).encode('utf-8'))
-    print(messageString)
-    # print(msg)
+    print("NOTE: {}".format(note - 53))
+    onOff = msg.type == 'note_on'
+    (message_1, message_2) = modMessages(message_1, message_2, note, onOff)
 
-USB.write(defaultString.encode('utf-8'))
+    USB.write(struct.pack('>LL', message_1, message_2))
+    print(message_1)
+    print(message_2)
+    print(msg)
+
+# USB.write(defaultString.encode('utf-8'))
