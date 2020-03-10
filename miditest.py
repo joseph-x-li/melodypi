ad97cf9046a2f78a29c98b660dc0d58baa615c6e
Joseph Li <13826851+joseph-x-li@users.noreply.github.com>
2020-03-10 01:21:59 -0400

    improvemenets

--- miditest.py
+++ miditest.py
@@ -1,12 +1,13 @@
 import mido
 from mido import MidiFile
 import serial
+from pprint import pprint
 
 mid = MidiFile('../Marunouchi.mid')
 port = "/dev/ttyUSB0"
 
 defaultString = "711111111111111111111111111111111111111"
-messageString = "711111111111111111111111111111111111111"
+messageString = list("700000000000000000000000000000000000000")
 
 x="700000000000000000000000000000000000000"
 
@@ -16,7 +17,7 @@ USB.flushInput()
 USB.write(defaultString.encode('utf-8'))
 
 for msg in mid.play():
-    if msg.is_meta:
+    if not (msg.type == 'note_on' or msg.type == 'note_off'):
         continue
     note = msg.note
     if not (note >=53 and note <=89): # ONLY NOTES F3(53) => F6(89)
@@ -25,8 +26,8 @@ for msg in mid.play():
         messageString[note-52] = '1'
     else:
         messageString[note-52] = '0'
-    USB.write(messageString.encode('utf-8'))
+    USB.write((''.join(messageString)).encode('utf-8'))
     print(messageString)
-    print(msg)
+    # print(msg)
 
 USB.write(defaultString.encode('utf-8'))
